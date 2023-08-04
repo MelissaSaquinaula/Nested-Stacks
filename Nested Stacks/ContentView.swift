3206592ea0cf69cf01b4deec1abd54b19ee2ca28
Melissa Saquinaula <melissasaquinaula@gmail.com>
2023-08-04 12:55:34 -0400

    Initial Commit

---
+++ Nested Stacks/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Nested Stacks
+//
+//  Created by scholar on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(.systemPurple)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+            Image("joy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+            
+        HStack(spacing: 20.0) {
+            Text("Joy Buolamini")
+                        .font(.title)
+                        .fontWeight(.bold)
+            Text("Poet of Code")
+}
+                
+        Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+}
+            
+            .padding()
+        .background(Rectangle()
+            .foregroundColor(.white))
+        .cornerRadius(15)
+        .shadow(radius: 15)
+        .padding()
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nested Stacks/Nested_StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nested_StacksApp.swift
+//  Nested Stacks
+//
+//  Created by scholar on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Nested_StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
